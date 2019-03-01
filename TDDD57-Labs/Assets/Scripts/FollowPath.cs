cae53a084e744b120f31d6e3e11ff32cb02ba939
Tjelvar Guo <tjegu689@student.liu.se>
2019-03-01 11:56:00 +0100

    cube follows path

---
+++ TDDD57-Labs/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public PathEditor pathToFollow;
+
+    public int currentWayPointID = 0;
+    public float speed;
+    public float reachDistance = 1.0f;
+    public float rotationSpeed = .5f;
+    public string pathName;
+
+    private Vector3 previousPosition;
+    private Vector3 currentPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pathToFollow = GameObject.Find(pathName).GetComponent<PathEditor>();
+        previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 nextPoint = pathToFollow.pathNodeTransforms[currentWayPointID].position;
+        float distance = Vector3.Distance(nextPoint, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, nextPoint, Time.deltaTime * speed);
+
+        transform.LookAt(pathToFollow.pathNodeTransforms[currentWayPointID]);
+
+        if (distance <= reachDistance) {
+            currentWayPointID++;
+        }
+        
+        if (currentWayPointID >= pathToFollow.pathNodeTransforms.Count) {
+            currentWayPointID = 0;
+        }
+    }
+}
---
+++ TDDD57-Labs/Assets/Scripts/PathEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathEditor : MonoBehaviour
+{
+    public Color pathColor;
+    public List<Transform> pathNodeTransforms = new List<Transform>();
+
+    private Transform[] children;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = pathColor;
+        pathNodeTransforms.Clear();
+        children = GetComponentsInChildren<Transform>();
+
+        foreach (Transform nodeTransfrom in children) {
+            // If statement makes sure not to add the parent's transform to the path
+            if (nodeTransfrom != this.transform) {
+                pathNodeTransforms.Add(nodeTransfrom);
+            }
+        }
+        Debug.Log(pathNodeTransforms.Count);
+        for (int i = 0; i < pathNodeTransforms.Count; i++) {
+            Vector3 currentPosition = pathNodeTransforms[i].position;
+            if (i > 0) {
+                Vector3 previousPosition = pathNodeTransforms[i-1].position;
+                Gizmos.DrawLine(previousPosition, currentPosition);
+                Gizmos.DrawWireSphere(currentPosition, .15f);
+            }
+        }
+    }
+
+}
