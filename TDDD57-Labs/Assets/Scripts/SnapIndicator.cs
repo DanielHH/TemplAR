9bed169bbaadaac9c0b2aaff5d998634fcce308a
Tjelvar Guo <tjegu689@student.liu.se>
2019-03-05 15:45:24 +0100

    add snapp indicator system visuals

---
+++ TDDD57-Labs/Assets/Scripts/SnapIndicator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapIndicator : MonoBehaviour
+{
+    public float fadeSpeed = 200;
+    [Range(0f, 255f)]
+    public float opacityRange = 200;
+
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = new Color32(255, 255, 255, (byte)Mathf.PingPong(Time.time * fadeSpeed, opacityRange));
+    }
+}
---
+++ TDDD57-Labs/Assets/Scripts/SnapTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapTrigger : MonoBehaviour
+{
+    public GameObject snappedIndicatorPrefab;
+
+    private bool indicatorPlaced = false;
+    private GameObject snappedIndicator;
+    private Collider collisionObjectCollider;
+
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player" && !indicatorPlaced) {
+            collisionObjectCollider = other;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player" && !indicatorPlaced) {
+            collisionObjectCollider = null;
+        }
+    }
+
+    void Update() {
+        if (collisionObjectCollider != null) {
+            Vector3 center = collisionObjectCollider.bounds.center;
+            if (!indicatorPlaced) {
+                if (GetComponent<BoxCollider>().bounds.Contains(center)) {
+                    snappedIndicator = Instantiate(snappedIndicatorPrefab, transform.position, Quaternion.identity);
+                    indicatorPlaced = true;
+                }
+            } else {
+                if (!GetComponent<BoxCollider>().bounds.Contains(center)) {
+                    Destroy(snappedIndicator);
+                    indicatorPlaced = false;
+                }
+            }          
+        }
+    }
+}
