8c661a17ad8862376a469c0610aba4c1f8b1b703
Tjelvar Guo <tjegu689@student.liu.se>
2019-03-13 14:53:52 +0100

    merge reset button branch to add reset level func

--- TDDD57-Labs/Assets/Scripts/GameManager.cs
+++ TDDD57-Labs/Assets/Scripts/GameManager.cs
@@ -4,23 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
-
-    public TMPro.TMP_Text debug;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void ResetLevel() {
-        debug.SetText("NUUGG");
+
         GameObject level = GameObject.FindGameObjectWithTag("LevelObject");
         Destroy(level);
     }
