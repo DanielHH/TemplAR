aae8136998da411af53a162b46d9b7f6d59ba447
Daniel Herzegh <danhe178@student.liu.se>
2019-03-14 10:05:52 +0100

    add rotationsnap-system

--- TDDD57-Labs/Assets/Scripts/SnapTrigger.cs
+++ TDDD57-Labs/Assets/Scripts/SnapTrigger.cs
@@ -5,6 +5,9 @@ public class SnapTrigger : MonoBehaviour
     public GameObject snappedIndicatorPrefab;
     public float proximityDistance = 1f;
     public WayPoint wayPoint;
+    public float angleLimit = 30f;
+
+    private TMPro.TextMeshProUGUI debug;
 
     private GameObject snappedIndicator;
     private Pickupable pickupableObject;
@@ -20,11 +23,11 @@ public class SnapTrigger : MonoBehaviour
 
     private void Start()  {
         gameObject.SetActive(false);
+        debug = GameObject.FindGameObjectWithTag("Debug").GetComponent<TMPro.TextMeshProUGUI>();
     }
 
     private void Update() {
         if (pickupableObject != null && pickupableObject.isSelected()) {
-
             for (int i = 0; i < pickupableObject.snapTriggers.Count; i++) {
                 if (pickupableObject.snapTriggers[i] == this) {
                     pickupableObject.setCurrentSnapTrigger(this);
@@ -34,9 +37,22 @@ public class SnapTrigger : MonoBehaviour
             }
 
             if (foundChild) {
-                Vector3 center = pickupableObject.GetComponent<Collider>().bounds.center;
+                Vector3 pickupableCenter = pickupableObject.GetComponent<Collider>().bounds.center;
+                Vector3 indicatorCenter = GetComponent<Collider>().bounds.center;
+                float pickupableYRotation = pickupableObject.transform.eulerAngles.y;
+                float indicatorYRotation = transform.eulerAngles.y;
+                float rotationDiff = Mathf.Abs(Mathf.DeltaAngle(pickupableYRotation, indicatorYRotation));
+                float symmRotationDiff;
+                if (pickupableObject.isSymmetric) {
+                    symmRotationDiff = Mathf.Abs(Mathf.DeltaAngle(pickupableYRotation + 180, indicatorYRotation));
+                } else {
+                    symmRotationDiff = rotationDiff;
+                }
+
+                //debug.SetText("pY " + pickupableYRotation + " iY " + indicatorYRotation + " rD " + rotationDiff + " sRD " + symmRotationDiff);
+
                 if (!indicatorPlaced) {
-                    if (Vector3.Distance(GetComponent<Collider>().bounds.center, center) <= proximityDistance) {
+                    if (Vector3.Distance(indicatorCenter, pickupableCenter) <= proximityDistance && (rotationDiff < angleLimit || symmRotationDiff < angleLimit)) {
                         if (wayPoint.wayPointType == WayPointType.DANGER) {
                             snappedIndicator = Instantiate(snappedIndicatorPrefab, transform.position, transform.rotation);
                             snappedIndicator.transform.parent = transform;
@@ -45,7 +61,7 @@ public class SnapTrigger : MonoBehaviour
                         }
                     }
                 } else {
-                    if (Vector3.Distance(GetComponent<Collider>().bounds.center, center) > proximityDistance) {
+                    if (Vector3.Distance(indicatorCenter, pickupableCenter) > proximityDistance || (rotationDiff >= angleLimit && symmRotationDiff >= angleLimit)) {
                         DestroyIndicator();
                         pickupableObject.UnSnap();
                     }
